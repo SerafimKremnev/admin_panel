3581208500a454565760e7a500c6c07650f54975
Serafim Kremnev <serafimkremnev1008@gmail.com>
2023-07-18 20:49:03 +0300

    добавил группировку задач

--- src/components/modal/TrackModal/TrackModal.tsx
+++ src/components/modal/TrackModal/TrackModal.tsx
@@ -5,6 +5,8 @@ import styles from './TrackModal.module.css'
 import {convertToTime} from "../../../utils/converIsoToTime";
 import {Typography} from "@mui/material";
 import TaskName from "../../elements/TaskName/TaskName";
+import Location from "../../elements/Location/Location";
+import cn from "classnames";
 
 
 interface TrackModalProps {
@@ -19,19 +21,24 @@ const TrackModal = ({timersToday, name, open, onClose}: TrackModalProps) => {
       <Typography fontSize={'2rem'} fontWeight={600}>{name}</Typography>
       <div className={styles.timers}>
         {
-          timersToday.slice(0, timersToday.length-1).map(timer =>
-            timer.task &&
-            <div
-              key={timer.task.id}
-              className={styles.timer}
-            >
-              <TaskName
-                href={timer.task.url}
-                name={timer.task.name}
-                status={timer.task.status}
-                currentTimeTrack={timer.duration}
-              />
-            </div>
+          timersToday.slice(1, timersToday.length).map((timer, i) => {
+              const isGroup = timer.task && i > 0 && timer.task.location.list.id !== timersToday.slice(1, timersToday.length)[i-1].task?.location.list.id
+              return (timer.task &&
+              <div
+                key={timer.task.id}
+                className={cn(styles.timer, !isGroup && styles.noMargin)}
+              >
+                {isGroup &&
+                  <Location location={timer.task.location}/>}
+                {i == 0 && <Location location={timer.task.location}/>}
+                <TaskName
+                  href={timer.task.url}
+                  name={timer.task.name}
+                  status={timer.task.status}
+                  currentTimeTrack={timer.duration}
+                />
+              </div>)
+            }
           )
         }
       </div>
