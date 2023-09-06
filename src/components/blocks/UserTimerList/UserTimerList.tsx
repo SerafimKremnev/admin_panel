a046cf69faa3432e319b2b2f5ecb94910c2225fa
Serafim Kremnev <serafimkremnev1008@gmail.com>
2023-09-06 18:03:41 +0300

    поправил таймерф

--- src/components/blocks/UserTimerList/UserTimerList.tsx
+++ src/components/blocks/UserTimerList/UserTimerList.tsx
@@ -1,51 +1,61 @@
-import React, {useEffect, useState} from 'react';
-import styles from './UserTimerList.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "./UserTimerList.module.css";
 import CardUserPreview from "../CardUserPreview/CardUserPreview";
-import {IUser} from "../../../types/timers.interface";
+import { IUser } from "../../../types/timers.interface";
 
 interface UserTimerListProps {
-  users: IUser[]
+  users: IUser[];
 }
 
-const UserTimerList = ({users}: UserTimerListProps) => {
-  const [userTrackNow, setUserTrackNow] = useState(users)
-  const [userSleep, setUserSleep] = useState(users)
+const UserTimerList = ({ users }: UserTimerListProps) => {
+  const [userTrackNow, setUserTrackNow] = useState(users);
+  const [userSleep, setUserSleep] = useState(users);
 
   useEffect(() => {
-    const trackNow = []
-    const trackSleep = []
+    const trackNow = [];
+    const trackSleep = [];
 
-    users.forEach(timer => {
-      if(timer.timersToday[0].end) {
-        trackSleep.push(timer)
+    users.forEach((timer) => {
+      if (timer.timersToday.at(-1).end) {
+        trackSleep.push(timer);
       } else {
-        trackNow.push(timer)
+        trackNow.push(timer);
       }
-    })
+    });
 
-    setUserTrackNow(trackNow)
-    setUserSleep(trackSleep)
-  }, [users])
+    setUserTrackNow(trackNow);
+    setUserSleep(trackSleep);
+  }, [users]);
 
   return (
     <div>
       <h3 className={styles.subtitle}>Трекают сейчас:</h3>
       <div className={styles.root}>
-        {userTrackNow.length ? userTrackNow.map((user) =>
-          <CardUserPreview
-            user={user}
-            key={user.clickupUser.id}
-            currentTimer={user.timersToday[0]}
-          />) : <span className={styles.null}>Никого нет</span>}
+        {userTrackNow.length ? (
+          userTrackNow.map((user) => (
+            <CardUserPreview
+              user={user}
+              key={user.clickupUser.id}
+              currentTimer={user.timersToday.at(-1)}
+            />
+          ))
+        ) : (
+          <span className={styles.null}>Никого нет</span>
+        )}
       </div>
       <h3 className={styles.subtitle}>Последние треки:</h3>
       <div className={styles.root}>
-        {userSleep.length ? userSleep.map((user) =>
-          <CardUserPreview
-            user={user}
-            key={user.clickupUser.id}
-            currentTimer={user.timersToday[0]}
-          />) : <span className={styles.null}>Никого нет</span>}
+        {userSleep.length ? (
+          userSleep.map((user) => (
+            <CardUserPreview
+              user={user}
+              key={user.clickupUser.id}
+              currentTimer={user.timersToday[0]}
+            />
+          ))
+        ) : (
+          <span className={styles.null}>Никого нет</span>
+        )}
       </div>
     </div>
   );
--- src/components/modal/TrackModal/TrackModal.tsx
+++ src/components/modal/TrackModal/TrackModal.tsx
@@ -1,50 +1,50 @@
-import React, {useEffect, useState} from 'react';
-import {TimersToday} from "../../../types/timers.interface";
+import React, { useEffect, useState } from "react";
+import { TimersToday } from "../../../types/timers.interface";
 import Modal from "../Modal/Modal";
-import styles from './TrackModal.module.css'
-import {convertToTime} from "../../../utils/converIsoToTime";
-import {Typography} from "@mui/material";
+import styles from "./TrackModal.module.css";
+import { convertToTime } from "../../../utils/converIsoToTime";
+import { Typography } from "@mui/material";
 import TaskName from "../../elements/TaskName/TaskName";
 import Location from "../../elements/Location/Location";
 import cn from "classnames";
 
-
 interface TrackModalProps {
-  timersToday: TimersToday[]
-  open: boolean
-  name: string
-  onClose: () => void
+  timersToday: TimersToday[];
+  open: boolean;
+  name: string;
+  onClose: () => void;
 }
-const TrackModal = ({timersToday, name, open, onClose}: TrackModalProps) => {
-  const [timers, setTimers] = useState<TimersToday[]>([])
-  useEffect(() => {
-    setTimers(timersToday.slice(1, timersToday.length))
-  }, [timersToday])
+const TrackModal = ({ timersToday, name, open, onClose }: TrackModalProps) => {
   return (
     <Modal isOpen={open} onRequestClose={onClose}>
-      <Typography fontSize={'2rem'} fontWeight={600}>{name}</Typography>
+      <Typography fontSize={"2rem"} fontWeight={600}>
+        {name}
+      </Typography>
       <div className={styles.timers}>
-        {
-          timers.map((timer, i) => {
-              const isGroup = timer.task && i > 0 && timer.task.location.list.id !== timers[i-1].task?.location.list.id
-              return (timer.task &&
+        {timersToday.map((timer, i) => {
+          const isGroup =
+            timer.task &&
+            i > 0 &&
+            timer.task.location.list.id !==
+              timersToday[i - 1].task?.location.list.id;
+          return (
+            timer.task && (
               <div
                 key={i}
                 className={cn(styles.timer, !isGroup && styles.noMargin)}
               >
-                {isGroup &&
-                  <Location location={timer.task.location}/>}
-                {i == 0 && <Location location={timer.task.location}/>}
+                {isGroup && <Location location={timer.task.location} />}
+                {i == 0 && <Location location={timer.task.location} />}
                 <TaskName
                   href={timer.task.url}
                   name={timer.task.name}
                   status={timer.task.status}
                   currentTimeTrack={timer.duration}
                 />
-              </div>)
-            }
-          )
-        }
+              </div>
+            )
+          );
+        })}
       </div>
     </Modal>
   );
